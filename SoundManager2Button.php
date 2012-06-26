0c9cc63132d84bcd90d57a8aa8348ffd8be0a25b
kroocsiogsi <kroocsiogsi@localhost>
2012-06-26 21:35:28 +0000

    Bump version for behavior change.

--- SoundManager2Button.php
+++ SoundManager2Button.php
@@ -60,7 +60,7 @@ $wgExtensionCredits['media'][] = array(
 	'author'         => 'kroocsiogsi',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:SoundManager2Button',
 	'descriptionmsg' => 'soundmanager2button-desc',
-	'version'        => '0.3.2',
+	'version'        => '0.3.3',
 );
 
 $wgExtensionMessagesFiles['SoundManager2Button'] = dirname( __FILE__ ) . '/SoundManager2Button.i18n.php';
